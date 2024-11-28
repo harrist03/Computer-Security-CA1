097755a11449432edfb4444c15b31d7957c9dbed
harrist03 <152608673+harrist03@users.noreply.github.com>
2024-11-28 01:59:42 +0000

    scanner created, error checking on choices entered

--- Main.java
+++ Main.java
@@ -1,11 +1,40 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        int choice = 0;
+        boolean valid = true;
+
         String[] menu = { "========= Menu =========",
                 "1. Encrypt a file",
                 "2. Decrypt a file",
                 "3. Quit",
                 "========================" };
-        displayMenu(menu);
+        Scanner sc = new Scanner(System.in);
+        do {
+            displayMenu(menu);
+            if (sc.hasNextInt()) {
+                choice = sc.nextInt();
+                if (choice == 1) {
+                    System.out.println("1");
+                } else if (choice == 2) {
+                    System.out.println("2");
+                } else if (choice == 3) {
+                    valid = false;
+                    System.out.println("Goodbye!");
+                } else {
+                    System.out.println("\nInvalid input! Enter numbers 1-3.");
+                }
+            } else {
+                System.out.println("Invalid input! Please enter a number.");
+                sc.next(); // clear the invalid input
+            }
+        } while (valid);
+
+        // System.out.println("Enter file name (include .txt)");
+        // String fileName = sc.nextLine();
+
+        sc.close();
     }
 
     // Task 1: Create a menu system
