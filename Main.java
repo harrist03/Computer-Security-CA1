fba4886505992e51f3e4bc5d9b4cd1042008c3e4
harrist03 <152608673+harrist03@users.noreply.github.com>
2024-11-28 03:09:37 +0000

    random AES key generator method completed

--- Main.java
+++ Main.java
@@ -1,40 +1,47 @@
 import java.io.File;
 import java.util.Scanner;
 
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+
 public class Main {
     public static void main(String[] args) {
-        int choice = 0;
-        boolean valid = true;
-        String fileName = "";
+        try {
+            int choice = 0;
+            boolean valid = true;
+            String fileName = "";
 
-        String[] menu = { "========= Menu =========",
-                "1. Encrypt a file",
-                "2. Decrypt a file",
-                "3. Quit",
-                "========================" };
-        Scanner sc = new Scanner(System.in);
-        do {
-            displayMenu(menu);
-            if (sc.hasNextInt()) {
-                choice = sc.nextInt();
-                sc.nextLine();
-                if (choice == 1) {
-                    String ValidFileName = checkFileValid(sc);
-                } else if (choice == 2) {
-                    String ValidFileName = checkFileValid(sc);
-                } else if (choice == 3) {
-                    valid = false;
-                    System.out.println("Goodbye!");
+            String[] menu = { "========= Menu =========",
+                    "1. Encrypt a file",
+                    "2. Decrypt a file",
+                    "3. Quit",
+                    "========================" };
+            Scanner sc = new Scanner(System.in);
+            do {
+                displayMenu(menu);
+                if (sc.hasNextInt()) {
+                    choice = sc.nextInt();
+                    sc.nextLine();
+                    if (choice == 1) {
+                        // encryptFile(checkFileValid(sc));
+                    } else if (choice == 2) {
+                        String ValidFileName = checkFileValid(sc);
+                    } else if (choice == 3) {
+                        valid = false;
+                        System.out.println("Goodbye!");
+                    } else {
+                        System.out.println("\nInvalid input! Enter numbers 1-3.");
+                    }
                 } else {
-                    System.out.println("\nInvalid input! Enter numbers 1-3.");
+                    System.out.println("Invalid input! Please enter a number.");
+                    sc.next(); // clear the invalid input
                 }
-            } else {
-                System.out.println("Invalid input! Please enter a number.");
-                sc.next(); // clear the invalid input
-            }
-        } while (valid);
+            } while (valid);
 
-        sc.close();
+            sc.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     // Task 1: Create a menu system
@@ -48,7 +55,7 @@ public class Main {
         String fileName = "";
         File file;
         boolean isValid = false;
-    
+
         while (!isValid) {
             System.out.println("Enter file name (include .txt): ");
             fileName = sc.nextLine();
@@ -59,8 +66,14 @@ public class Main {
                 System.out.println("Invalid file name! Please enter a valid .txt file name.");
             }
         }
-    
+
         return fileName;
     }
-    
+
+    // Generate a random AES key
+    public static SecretKey generateKey() throws Exception {
+        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+        keyGenerator.init(128); // 128 bits
+        return keyGenerator.generateKey();
+    }
 }
